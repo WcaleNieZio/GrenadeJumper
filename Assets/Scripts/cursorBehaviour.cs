72d4062db9e51a574512c41b31069450851dd714
Adam <savander.pl@gmail.com>
2016-08-04 16:12:29 +0200

    Variables: +mouseDistance, +maxDistance. +Refactor names

--- Assets/Scripts/cursorBehaviour.cs
+++ Assets/Scripts/cursorBehaviour.cs
@@ -3,25 +3,26 @@ using System.Collections;
 
 public class cursorBehaviour : MonoBehaviour {
 
-	public float limitRadius;
+
 	public Transform cursor;
 	private Vector3 delta;
-	public bool mouseVisible;
 
-	// Use this for initialization
+	public bool mouseVisible;
+	public float mouseMaxDistance;
+	public float mouseSensitive;
+
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked;
 	}
-
-	// Update is called once per frame4
+		
 	void Update () {
 		Cursor.visible = mouseVisible;
 		cursorPosition ();
 	}
 
 	void cursorPosition(){
-		delta = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+		delta = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * (mouseSensitive / 10);
 		if(delta != Vector3.zero)
-			cursor.localPosition = Vector3.ClampMagnitude ((new Vector3 (cursor.position.x, cursor.position.y, 0) + delta) - transform.position, limitRadius);
+			cursor.localPosition = Vector3.ClampMagnitude ((new Vector3 (cursor.position.x, cursor.position.y, 0) + delta) - transform.position, mouseMaxDistance);
 	}
 }
