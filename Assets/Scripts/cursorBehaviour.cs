47762f6290a68a314a10a943d85e905ad7876402
Adam <savander.pl@gmail.com>
2016-08-04 15:39:20 +0200

    Cursor: finally done

--- Assets/Scripts/cursorBehaviour.cs
+++ Assets/Scripts/cursorBehaviour.cs
@@ -5,31 +5,23 @@ public class cursorBehaviour : MonoBehaviour {
 
 	public float limitRadius;
 	public Transform cursor;
-
-	private Vector3 lastMousePosition;
 	private Vector3 delta;
 	public bool mouseVisible;
 
 	// Use this for initialization
 	void Start () {
-		Cursor.lockState = CursorLockMode.Confined;
-		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	// Update is called once per frame4
 	void Update () {
 		Cursor.visible = mouseVisible;
-
 		cursorPosition ();
 	}
 
 	void cursorPosition(){
-
-		if (lastMousePosition != Input.mousePosition) {
-			delta = (Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position) - (Camera.main.ScreenToWorldPoint (lastMousePosition) - transform.position);
+		delta = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+		if(delta != Vector3.zero)
 			cursor.localPosition = Vector3.ClampMagnitude ((new Vector3 (cursor.position.x, cursor.position.y, 0) + delta) - transform.position, limitRadius);
-		}
-
-		lastMousePosition = Input.mousePosition;
 	}
 }
